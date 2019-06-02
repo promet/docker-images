c897e81ea63e68330e981fa00cbb75f57d60e1df
Lisa Ridley <lisa@prometsource.com>
2019-06-01 22:08:13 -0400

    Adding PHP 7.3 as a fastcgi service

--- docker/apache2/conf/bin/service.d/httpd.sh
+++ docker/apache2/conf/bin/service.d/httpd.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
+# Replace markers
+go-replace \
+    -s "<DOCUMENT_INDEX>" -r "$WEB_DOCUMENT_INDEX" \
+    -s "<DOCUMENT_ROOT>" -r "$WEB_DOCUMENT_ROOT" \
+    -s "<ALIAS_DOMAIN>" -r "$WEB_ALIAS_DOMAIN" \
+    -s "<SERVERNAME>" -r "$HOSTNAME" \
+    -s "<PHP_SOCKET>" -r "$WEB_PHP_SOCKET" \
+    -s "<PHP_TIMEOUT>" -r "$WEB_PHP_TIMEOUT" \
+    --path=/opt/docker/etc/httpd/ \
+    --path-pattern='*.conf' \
+    --ignore-empty
 
-source /opt/docker/provision/entrypoint.d/20-apache.sh
+if [[ -z "$WEB_PHP_SOCKET" ]]; then
+    ## WEB_PHP_SOCKET is not set, remove PHP files
+    rm -f -- /opt/docker/etc/httpd/conf.d/10-php.conf
+fi
+
+##source /opt/docker/provision/entrypoint.d/20-apache.sh
 
 # Init vars
 if [[ -z "$SERVICE_APACHE_OPTS" ]]; then SERVICE_APACHE_OPTS=""; fi
