1e6ddbfe66aab8a4a8b967a31ced2ef2d6aa71e5
Lisa Ridley <lisa@prometsource.com>
2019-06-03 17:00:22 -0400

    Adding support for MailHog, OpCache (php 7.2 and 7.3) and XDebug

--- docker/php/bin/php.sh
+++ docker/php/bin/php.sh
@@ -32,7 +32,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
     php<PHP_VERSION>-soap \
     php<PHP_VERSION>-tidy \
     php<PHP_VERSION>-uuid \
-    php<PHP_VERSION>-xdebug \
     php<PHP_VERSION>-xmlrpc \
     php<PHP_VERSION>-xsl \
     php<PHP_VERSION>-yaml \
@@ -40,7 +39,8 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
 && if [[ "$WEB_PHP_VERSION" == "7.1" ]]; then
 	DEBIAN_FRONTEND=noninteractive apt-get install -y \
     php7.1-ssh2 \
-    php7.1-mcrypt
+    php7.1-mcrypt \
+    php7.1-xdebug
 elif [[ "$WEB_PHP_VERSION" == "7.2" ]]; then
     DEBIAN_FRONTEND=noninteractive apt-get install -y php-pear \
         php<PHP_VERSION>-json \
@@ -68,8 +68,17 @@ elif [[ "$WEB_PHP_VERSION" == "7.2" ]]; then
         php<PHP_VERSION>-xml \
         php<PHP_VERSION>-xmlreader \
         php<PHP_VERSION>-xmlwriter \
+    && pecl install xdebug-2.7.0 \
 	&& pecl install mcrypt-1.0.1 \
-	&& bash -c "echo extension=mcrypt.so > /etc/php/7.2/fpm/conf.d/mcrypt.ini"
+    && { \
+        echo 'opcache.memory_consumption=128'; \
+        echo 'opcache.interned_strings_buffer=8'; \
+        echo 'opcache.max_accelerated_files=4000'; \
+        echo 'opcache.revalidate_freq=0'; \
+        echo 'opcache.fast_shutdown=1'; \
+        echo 'opcache.enable_cli=1'; \
+    } > /etc/php/<PHP_VERSION>/fpm/conf.d/20-opcache.ini \
+    && bash -c "echo extension=mcrypt.so > /etc/php/7.2/fpm/conf.d/mcrypt.ini"
 elif [[ "$WEB_PHP_VERSION" == "7.3" ]]; then
     DEBIAN_FRONTEND=noninteractive apt-get install -y php-pear \
         php<PHP_VERSION>-json \
@@ -99,9 +108,38 @@ elif [[ "$WEB_PHP_VERSION" == "7.3" ]]; then
         php<PHP_VERSION>-xml \
         php<PHP_VERSION>-xmlreader \
         php<PHP_VERSION>-xmlwriter \
-	&& pecl install mcrypt-1.0.2
-	bash -c "echo extension=mcrypt.so > /etc/php/7.3/fpm/conf.d/mcrypt.ini"
+    && pecl install xdebug-2.7.0 \
+	&& pecl install mcrypt-1.0.2 \
+    && bash -c "echo extension=mcrypt.so > /etc/php/7.3/fpm/conf.d/40-mcrypt.ini" \
+    && { \
+        echo 'opcache.memory_consumption=128'; \
+        echo 'opcache.interned_strings_buffer=8'; \
+        echo 'opcache.max_accelerated_files=4000'; \
+        echo 'opcache.revalidate_freq=0'; \
+        echo 'opcache.fast_shutdown=1'; \
+        echo 'opcache.enable_cli=1'; \
+    } > /etc/php/<PHP_VERSION>/fpm/conf.d/20-opcache.ini
 fi
 
-sed -i -e 's/listen = \/run\/php\/php<PHP_VERSION>-fpm.sock/listen = php:9000/g' /etc/php/<PHP_VERSION>/fpm/pool.d/www.conf
+sed -i -e 's/listen = \/run\/php\/php<PHP_VERSION>-fpm.sock/listen = <PHP_SOCKET>/g' /etc/php/<PHP_VERSION>/fpm/pool.d/www.conf
 sed -i -e 's/;daemonize = yes/daemonize = no/g' /etc/php/<PHP_VERSION>/fpm/php-fpm.conf
+
+{ \
+    echo '; Mailhog php.ini settings for Expresso PHP.';
+    echo 'sendmail_path = "/opt/go/bin/mhsendmail --smtp-addr=mailhog:1025"';
+} > /etc/php/<PHP_VERSION>/fpm/conf.d/20-mailhog.ini
+
+{ \
+    echo '[xdebug]'; \
+    echo 'zend_extension=xdebug.so'; \
+    echo 'xdebug.profiler_enable=0'; \
+    echo 'xdebug.default_enable=1'; \
+    echo 'xdebug.remote_enable=1'; \
+    echo 'xdebug.remote_handler=dbgp'; \
+    echo '; port 9000 is used by php-fpm'; \
+    echo 'xdebug.remote_port=9000'; \
+    echo 'xdebug.remote_autostart=0'; \
+    echo 'xdebug.remote_connect_back=0'; \
+    echo 'xdebug.idekey="PHPSTORM"'; \
+    echo 'xdebug.remote_host=10.254.254.254'; \
+} > /etc/php/<PHP_VERSION>/fpm/conf.d/20-xdebug.ini
